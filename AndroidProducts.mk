58f963c30cddc7fbb302941f809a9a856ad7cf3f
Oscar Greave <oscargreave04@gmail.com>
2026-02-01 03:40:55 -0300

    avicii: PixelOS bringup
    Signed-off-by: Oscar Greave <oscargreave04@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_avicii.mk
+    $(LOCAL_DIR)/custom_avicii.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_avicii-eng \
-    lineage_avicii-user \
-    lineage_avicii-userdebug
+    custom_avicii-eng \
+    custom_avicii-user \
+    custom_avicii-userdebug
