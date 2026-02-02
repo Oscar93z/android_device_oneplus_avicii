88a967f548fff6f72402e4db2313cd92ff356f14
Arian <arian.kulmer@web.de>
2026-02-02 00:53:50 -0300

    avicii: Stop including vendor/debugfs.config We disable debug fs in the defconfigs directly since I01a4dd199a357dd85838c071020bf966079a2092
    Change-Id: Ifcf9dd9d7a48ee93999c783f0f361e75bb97e364

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,7 +107,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := BRAND_SHOW_FLAG=oneplus
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm7250
-TARGET_KERNEL_CONFIG := vendor/lito-perf_defconfig vendor/debugfs.config vendor/oplus.config
+TARGET_KERNEL_CONFIG := vendor/lito-perf_defconfig vendor/oplus.config
 
 # Kernel Clang Flags
 KERNEL_CC := CC=clang
